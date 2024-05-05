3b248bec3c3de366f579c83fc5d68e4571c5bb85
Miguel Gómez <95737291+MiguelGmz7@users.noreply.github.com>
2024-05-05 08:44:38 +0000

    Funciona la prueba del modelo y la matriz
    despues de que cambialos los valores de etnia e industria a valores
    numericos podemos obervar que ahora el modelo los entiende de mejor manera

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
-from sklearn import datasets
 
-# for dataset in dir(datasets):
-#     print(dataset)
+# # for dataset in dir(datasets):
+# #     print(dataset)
 
-iris = datasets.load_iris()
-iris.keys()
-print(iris.DESCR)
+# iris = datasets.load_iris()
+# iris.keys()
+# print(iris.DESCR)
+
+# importamos pandas para poder leer el dataset
+import pandas as pd
+file_path = "BankOfAmericaCredits.csv"
+df = pd.read_csv(file_path)
+
+# definimos una variable "target" (x es el resto de las columnas, "y" la variable que queremos conocer)
+target = "Approved"
+x = df.drop(target, axis=1)
+y = df[target]
+
+# seleccionamos nuestros conjuntos de entrenamiento y prueba 
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+
+# creamos un algoritmo
+from sklearn.naive_bayes import GaussianNB
+method = GaussianNB()
+
+
+method.fit(x_train,y_train)
+
+#predecimos las clases de estos datos
+y_pred = method.predict(x_test)
+
+from sklearn.metrics import confusion_matrix
+
+matriz = confusion_matrix(y_test,y_pred)
+print(matriz)
+
+
+
